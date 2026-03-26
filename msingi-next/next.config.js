3a0643c6e9c6f3e0f3b96652be2f6d9117921171
Lilian Kwamboka <kwambokalilian59@gmail.com>
2026-03-26 23:57:35 +0300

    Ignore errors

--- msingi-next/next.config.js
+++ msingi-next/next.config.js
@@ -1,16 +1,21 @@
-import type { NextConfig } from "next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return [];
   },
+
+ // Ignore ESLint errors
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+  // Ignore TypeScript errors
   typescript: {
     ignoreBuildErrors: true,
   },
+
+  // Optional: reduce strict warnings
+  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
